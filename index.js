862a58d95d9f7221d0a5bdd96bc2b5736ddb0bd8
Mark Voorberg <mark@binaryops.ca>
2017-11-10 13:51:11 -0800

    fixed up the tests and a few bugs

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const { Schema } = require('mongoose');
-const transomNonceSchema = require('./lib/transomNonceSchema');
-const auditablePlugin = require('@transomjs/transom-mongoose/plugins/auditablePlugin');
+const transomNonceSchema = require('./lib/nonceSchema');
+const auditablePlugin = require('@transomjs/transom-mongoose/lib/plugins/auditablePlugin');
 const NonceHandler = require('./lib/nonceHandler');
 
 function TransomNonce() {
     this.initialize = function (server, options) {
         const mongoose = server.registry.get('mongoose');
-		mongoose.model('TransomAclUser', transomNonceSchema.NonceSchema());
+		mongoose.model('TransomNonce', transomNonceSchema.NonceSchema());
 
         const nonceHandler = new NonceHandler(server, options);
-        server.registry.set('nonce', nonceHandler);
+        server.registry.set('transomNonce', nonceHandler);
     }
 }
 
--- test/nonceHandler.spec.js
+++ test/nonceHandler.spec.js
@@ -6,54 +6,74 @@ const NonceHandler = require('../lib/nonceHandler');
 const mongoose = require('mongoose');
 const PocketRegistry = require('pocket-registry');
 const mongotest = require('./mongotest');
+const TransomNonce = require('../index');
 
 
 describe('NonceHandler', function (done) {
+
+    const server = {};
+
+    before(function () {
+        server.registry = new PocketRegistry();
+        mongoose.Promise = Promise;
+        server.registry.set('mongoose', mongoose);
+
+        const options = {};
+        TransomNonce.initialize(server, options);
+    });
 
     beforeEach(mongotest.prepareDb('mongodb://localhost/transomnoncetests', {
         timeout: 10000
     }));
     afterEach(mongotest.disconnect());
 
-    // beforeEach(function (done) {
-
-    //     // Stub out a mock nodemailer
-    //     // sinon.stub(nodemailer, 'createTransport').callsFake(function (opts) {
-    //     //     // whatever you would like innerLib.toCrazyCrap to do under test
-    //     //     function Transport() {
-    //     //         this.sendMail = function (opts, cb) {
-    //     //             if (cb) {
-    //     //                 return cb(opts.error, opts.response);
-    //     //             }
-    //     //         };
-    //     //         this.close = sinon.spy();
-    //     //     };
-    //     //     return new Transport();
-    //     // });
-    //     debug('Before each', this);
-    //     mongotest.prepareDb.call(this, 'mongodb://localhost/transomnoncetests')(done);
-    // });
-
-    // afterEach(function (done) {
-    //     // restore original functionality
-    //     // nodemailer.createTransport.restore();
-    //     debug('After each');
-    //     mongotest.disconnect(done);
-    // });
-
-    it('can call createTransport', function () {
-        const server = {};
-        server.registry = new PocketRegistry();
-        const dummyOptions = {};
-        // const transomNonce = new NonceHandler(server, dummyOptions);
-        debug(' ** each');
-
-        expect(true, "I should write some tests!");
-
-        // transomSmtp.sendMail({
-        //     from: "noreply@transomjs",
-        //     html: "Hello"
-        // });
-        // sinon.assert.calledOnce(nodemailer.createTransport);
+    it('has been registered with the server', function () {
+        const nonceHandler = server.registry.get('transomNonce');
+        expect(nonceHandler).to.be.an.instanceOf(Object);
+        expect(nonceHandler.createNonce).to.be.an.instanceOf(Function);
+        expect(nonceHandler.verifyNonce).to.be.an.instanceOf(Function);
     });
+
+    it('can create aand verify a Nonce', function () {
+        const nonceHandler = server.registry.get('transomNonce');
+
+        const payload = {
+            foo: 123
+        };
+        const expirySeconds = 1;
+        nonceHandler.createNonce(payload, expirySeconds, function (err, nonce) {
+            expect(err).to.be.null;
+            nonceHandler.verifyNonce(nonce.token, function (err, verifiedPayload) {
+                expect(err).to.be.null;
+                expect(verifiedPayload).to.eql(payload);
+            })
+        });
+    });
+
+    it('can create an expired nonce, which triggers an error on Validate', function () {
+        const nonceHandler = server.registry.get('transomNonce');
+
+        const payload = {
+            foo: 123
+        };
+        const expiredSeconds = -1;
+        nonceHandler.createNonce(payload, expiredSeconds, function (err, nonce) {
+            expect(err).to.be.null;
+            nonceHandler.verifyNonce(nonce.token, function (err, verifiedPayload) {
+                expect(err).to.be.not.null; // Invalid Token
+                expect(verifiedPayload).to.be.undefined;
+            })
+        });
+    });
+
+    it('triggers an error when called with a garbage token', function () {
+        const nonceHandler = server.registry.get('transomNonce');
+
+        nonceHandler.verifyNonce('I am an invalid token!', function (err, verifiedPayload) {
+            expect(err).to.be.not.null; // Invalid Token
+            expect(err.message).to.equal('Invalid Token');
+            expect(verifiedPayload).to.be.undefined;
+        })
+    });
+
 });
